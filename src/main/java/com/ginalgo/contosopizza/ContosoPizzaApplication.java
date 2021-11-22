855d1d83ebe063f9be680418fe5bd1f707a741eb
ginalig <aanigay@edu.hse.ru>
2021-11-22 21:08:15 +0300

    Init

---
+++ src/main/java/com/ginalgo/contosopizza/ContosoPizzaApplication.java
@@ -0,0 +1,13 @@
+package com.ginalgo.contosopizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContosoPizzaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContosoPizzaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ginalgo/contosopizza/ContosoPizzaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ginalgo.contosopizza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContosoPizzaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
